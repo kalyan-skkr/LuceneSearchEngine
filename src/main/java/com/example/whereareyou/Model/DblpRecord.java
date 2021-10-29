be189dd7586028a324c2ca524a6c5056d12cd143
kalyansabbella <kalyan_sabbella@outlook.com>
2021-10-29 18:12:57 -0400

    Search Enhancement

--- src/main/java/com/example/whereareyou/Model/DblpRecord.java
+++ src/main/java/com/example/whereareyou/Model/DblpRecord.java
@@ -1,5 +1,7 @@
 package com.example.whereareyou.Model;
 
+import java.util.List;
+
 public class DblpRecord {
     private int id;
 
@@ -20,6 +22,204 @@ public class DblpRecord {
     public void setEntry(Entry entry) {
         this.entry = entry;
     }
+    private Title title;
 
+    public Title getTitle() {
+        return title;
+    }
 
+    public void setTitle(Title title) {
+        this.title = title;
+    }
+
+    private List<Author> authorList;
+
+    public List<Author> getAuthorList() {
+        return authorList;
+    }
+
+    public void setAuthorList(List<Author> authorList) {
+        this.authorList = authorList;
+    }
+
+    private String year;
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+    private String school;
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+    private Publisher publisher;
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+    private String number;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+    private String pages;
+
+    public String getPages() {
+        return pages;
+    }
+
+    public void setPages(String pages) {
+        this.pages = pages;
+    }
+    private ISBN isbn;
+
+    public ISBN getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(ISBN isbn) {
+        this.isbn = isbn;
+    }
+    private EE ee;
+
+    public EE getEe() {
+        return ee;
+    }
+
+    public void setEe(EE ee) {
+        this.ee = ee;
+    }
+    private String month;
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+    private Series series;
+
+    public Series getSeries() {
+        return series;
+    }
+
+    public void setSeries(Series series) {
+        this.series = series;
+    }
+    private String volume;
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+    private List<Note> notes;
+
+    public List<Note> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+    }
+    private String bookTitle;
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+    private String crossRef;
+
+    public String getCrossRef() {
+        return crossRef;
+    }
+
+    public void setCrossRef(String crossRef) {
+        this.crossRef = crossRef;
+    }
+    private Url url;
+
+    public Url getUrl() {
+        return url;
+    }
+
+    public void setUrl(Url url) {
+        this.url = url;
+    }
+    private Editor editor;
+
+    public Editor getEditor() {
+        return editor;
+    }
+
+    public void setEditor(Editor editor) {
+        this.editor = editor;
+    }
+    private Cite cite;
+
+    public Cite getCite() {
+        return cite;
+    }
+
+    public void setCite(Cite cite) {
+        this.cite = cite;
+    }
+    private String cdRom;
+
+    public String getCdRom() {
+        return cdRom;
+    }
+
+    public void setCdRom(String cdRom) {
+        this.cdRom = cdRom;
+    }
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    private String chapter;
+
+    public String getChapter() {
+        return chapter;
+    }
+
+    public void setChapter(String chapter) {
+        this.chapter = chapter;
+    }
+    private String publnr;
+
+    public String getPublnr() {
+        return publnr;
+    }
+
+    public void setPublnr(String publnr) {
+        this.publnr = publnr;
+    }
 }
--- src/main/java/com/example/whereareyou/Search.java
+++ src/main/java/com/example/whereareyou/Search.java
@@ -1,6 +1,9 @@
 package com.example.whereareyou;
 
 import com.example.whereareyou.Constants.Constants;
+import com.example.whereareyou.Model.DblpRecord;
+import com.example.whereareyou.Model.Entry;
+import com.example.whereareyou.Model.Title;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
@@ -13,8 +16,11 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.FSDirectory;
 
+import java.io.IOException;
 import java.nio.file.Paths;
+import java.sql.Array;
 import java.sql.PreparedStatement;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -26,22 +32,40 @@ public class Search {
         QueryParser parser = new QueryParser("title", analyzer);
         Query query = parser.parse(searchQuery);
         TopDocs results = searcher.search(query,5);
+        List<DblpRecord> recordList = GetRecords(results, searcher);
         System.out.println(results.totalHits + " total matching documents");
-        for (int i = 0; i < results.scoreDocs.length; i++) {
-            Document doc = searcher.doc(results.scoreDocs[i].doc);
-            String path = doc.get("path");
-            System.out.println((i + 1) + ". " + path);
-            String title = doc.get("title");
-            if (title != null) {
-                System.out.println("   Title: " + doc.get("title"));
-            }
-            List<IndexableField> v = doc.getFields();
-            for ( IndexableField field : v )
-            {
-                System.out.println("        " + field.name()+": "+doc.get(field.name()));
-            }
-
-        }
+        System.out.println(recordList.stream().findFirst().get().getEntry().getValue());
+        System.out.println(recordList.stream().findFirst().get().getTitle().getValue());
+//        for (int i = 0; i < results.scoreDocs.length; i++) {
+//            Document doc = searcher.doc(results.scoreDocs[i].doc);
+//            String path = doc.get("path");
+//            System.out.println((i + 1) + ". " + path);
+//            String title = doc.get("title");
+//            if (title != null) {
+//                System.out.println("   Title: " + doc.get("title"));
+//            }
+//            List<IndexableField> v = doc.getFields();
+//            for ( IndexableField field : v )
+//            {
+//                System.out.println("        " + field.name()+": "+doc.get(field.name()));
+//            }
+//
+//        }
         reader.close();
+    }
+    private List<DblpRecord> GetRecords(TopDocs results, IndexSearcher searcher) throws IOException {
+        List<DblpRecord> recordList = new ArrayList<DblpRecord>();
+        for(int i = 0; i < results.scoreDocs.length; i++){
+            Document doc = searcher.doc(results.scoreDocs[i].doc);
+            Entry entry = new Entry(doc.get("entryType"), doc.get("entryType.mdate"),doc.get("entryType.key"),doc.get("entryType.publtype"), doc.get("entryType.cdate"));
+            Title title = new Title(doc.get("title"), doc.get("title.bibTex"));
+            DblpRecord rec = new DblpRecord();
+            rec.setId(Integer.parseInt(doc.get("docId")));
+            rec.setEntry(entry);
+            rec.setTitle(title);
+
+            recordList.add(rec);
+        }
+        return recordList;
     }
 }
